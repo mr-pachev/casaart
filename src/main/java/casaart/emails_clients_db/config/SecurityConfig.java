0ef8224576fc7a95351e27c1c2ecd80a090eecf7
Ivan Pachev <mr.pachev@abv.bg>
2024-11-17 13:27:16 +0200

    Added SecurityConfig.

---
+++ src/main/java/casaart/emails_clients_db/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package casaart.emails_clients_db.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        authorizeRequest -> {
+                            authorizeRequest
+                                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                    .requestMatchers("/", "/login", "/registration", "/login-error").permitAll()
+                                    .anyRequest().authenticated();
+                        }
+                )
+                .formLogin(
+                        formLogin -> {
+                            formLogin.loginPage("/login");
+                            formLogin.usernameParameter("username");
+                            formLogin.passwordParameter("password");
+                            formLogin.defaultSuccessUrl("/", true);
+                            formLogin.failureUrl("/login-error");
+                        }
+                )
+                .logout(
+                        logout -> {
+                            logout.logoutUrl("/logout");
+                            logout.logoutSuccessUrl("/index");
+                            logout.invalidateHttpSession(true);
+                        }
+                )
+                .build();
+    }
+}
