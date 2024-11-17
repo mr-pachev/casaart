1e4e007b63f7b29104d17a065da74bf676b00c3f
Ivan Pachev <mr.pachev@abv.bg>
2024-11-17 14:59:20 +0200

    Added Client entity.

--- src/main/java/casaart/emails_clients_db/model/entity/Client.java
+++ src/main/java/casaart/emails_clients_db/model/entity/Client.java
@@ -1,15 +1,101 @@
 package casaart.emails_clients_db.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import casaart.emails_clients_db.model.enums.SourceType;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "clients")
 public class Client extends BaseEntity{
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "middle_name")
     private String middleName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "first_name")
     private String companyName;
+    @Column(name = "email")
     private String email;
+    @Column(name = "phone_number")
     private int phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private SourceType sourceType;
+    @Column(name = "creat_date")
+    private LocalDate creatDate;
+    @Column(name = "modify_date")
+    private LocalDate modifyDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public SourceType getSourceType() {
+        return sourceType;
+    }
+
+    public void setSourceType(SourceType sourceType) {
+        this.sourceType = sourceType;
+    }
+
+    public LocalDate getCreatDate() {
+        return creatDate;
+    }
+
+    public void setCreatDate(LocalDate creatDate) {
+        this.creatDate = creatDate;
+    }
+
+    public LocalDate getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(LocalDate modifyDate) {
+        this.modifyDate = modifyDate;
+    }
 }
