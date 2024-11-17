b3a2060e8025b431264c258ee21e2f2a414bee0a
Ivan Pachev <mr.pachev@abv.bg>
2024-11-17 19:48:09 +0200

    Added AddUserDTO.

---
+++ src/main/java/casaart/emails_clients_db/model/dto/AddUserDTO.java
@@ -0,0 +1,84 @@
+package casaart.emails_clients_db.model.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class AddUserDTO {
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String username;
+    @NotBlank
+    @Size(min = 4, max = 12)
+    private String password;
+    @NotBlank
+    private String confirmPassword;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String firstName;
+    @Size(min = 2, max = 15)
+    private String middleName;
+    @Size(min = 3, max = 15)
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    @Size(min = 3, max = 15)
+    private String located;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLocated() {
+        return located;
+    }
+
+    public void setLocated(String located) {
+        this.located = located;
+    }
+}
--- src/main/java/casaart/emails_clients_db/web/UserController.java
+++ src/main/java/casaart/emails_clients_db/web/UserController.java
@@ -1,11 +1,71 @@
 package casaart.emails_clients_db.web;
 
+import casaart.emails_clients_db.model.dto.AddUserDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
+    @ModelAttribute("addUserDTO")
+    public AddUserDTO addUserDTO() {
+        return new AddUserDTO();
+    }
+
+    @ModelAttribute("loginUserDTO")
+    public LoginUserDTO loginUserDTO() {
+        return new LoginUserDTO();
+    }
+
+    //create new user
+    @GetMapping("/registration")
+    public String viewAddUserForm(Model model){
+
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(
+            @Valid AddUserDTO addUserDTO,
+            BindingResult bindingResult,
+            RedirectAttributes rAtt) {
+
+        if (bindingResult.hasErrors()) {
+            rAtt.addFlashAttribute("addUserDTO", addUserDTO);
+            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.addUserDTO", bindingResult);
+
+            return "redirect:/registration";
+        }
+
+        if (userService.isExistUser(addUserDTO.getUsername()) ||
+                !employeeService.isExistEmployeeByIN(addUserDTO.getIdentificationNumber())) {
+            rAtt.addFlashAttribute("addUserDTO", addUserDTO);
+            rAtt.addFlashAttribute("noAddedUser", true);
+            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.addUserDTO", bindingResult);
+
+            return "redirect:/registration";
+        }
+
+        boolean confirmPassword = addUserDTO.getPassword().equals(addUserDTO.getConfirmPassword());
+
+        if (!confirmPassword) {
+            rAtt.addFlashAttribute("addUserDTO", addUserDTO);
+            rAtt.addFlashAttribute("unconfirmed", true);
+            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.addUserDTO", bindingResult);
+
+            return "redirect:/registration";
+        }
+
+        userService.addUser(addUserDTO);
+
+        return "redirect:/login";
+    }
+
     //login
     @GetMapping("/login")
     public String viewLogin(){
