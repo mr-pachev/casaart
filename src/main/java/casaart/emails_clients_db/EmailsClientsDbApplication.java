9cd0b0c52ac2879faaaff30695e87681b15408dd
Ivan Pachev <mr.pachev@abv.bg>
2024-11-17 13:10:23 +0200

    Added emails_clients repo.

---
+++ src/main/java/casaart/emails_clients_db/EmailsClientsDbApplication.java
@@ -0,0 +1,13 @@
+package casaart.emails_clients_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailsClientsDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailsClientsDbApplication.class, args);
+    }
+
+}
