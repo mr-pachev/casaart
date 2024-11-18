81e501a781dfebe8acba7da4cdf5c603d4c27d71
Ivan Pachev <mr.pachev@abv.bg>
2024-11-18 19:35:42 +0200

    Added AddClientDTO.

--- src/main/java/casaart/emails_clients_db/model/dto/AddClientDTO.java
+++ src/main/java/casaart/emails_clients_db/model/dto/AddClientDTO.java
@@ -1,5 +1,85 @@
 package casaart.emails_clients_db.model.dto;
 
-public class AddClientDTO {
+import casaart.emails_clients_db.model.enums.SourceType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
 
+public class AddClientDTO {
+    @NotBlank
+    @Size(min = 2, max = 15)
+    private String firstName;
+    @NotBlank
+    @Size(min = 2, max = 15)
+    private String middleName;
+    @NotBlank
+    @Size(min = 2, max = 15)
+    private String lastName;
+    @NotBlank
+    @Size(min = 2, max = 15)
+    private String companyName;
+    @NotBlank@Email
+    private String email;
+    @NotNull
+    @Min(10)
+    private int phoneNumber;
+    @NotBlank
+    private String sourceType;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSourceType() {
+        return sourceType;
+    }
+
+    public void setSourceType(String sourceType) {
+        this.sourceType = sourceType;
+    }
 }
