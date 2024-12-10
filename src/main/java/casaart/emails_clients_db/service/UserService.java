b56c40d76bc602bb08d985632f8e3d285b608136
mr-pachev <mr.pachev@abv.bg>
2024-12-10 15:31:19 +0200

    Added sort by name option.

--- src/main/java/casaart/emails_clients_db/service/UserService.java
+++ src/main/java/casaart/emails_clients_db/service/UserService.java
@@ -14,6 +14,6 @@ public interface UserService {
     UserDTO findUserById(long id);
     //add new user
     void addUser(AddUserDTO addUserDTO);
-    //edit user
+    //edit usera
     void editUser(UserDTO userDTO);
 }
