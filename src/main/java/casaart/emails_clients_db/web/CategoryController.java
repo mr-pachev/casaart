e9f7499dc8e4dcf6842e1f3942b777b8b3b9d346
mr-pachev <mr.pachev@abv.bg>
2024-12-20 18:26:03 +0200

    Added add-category template.

--- src/main/java/casaart/emails_clients_db/web/CategoryController.java
+++ src/main/java/casaart/emails_clients_db/web/CategoryController.java
@@ -1,12 +1,18 @@
 package casaart.emails_clients_db.web;
 
 import casaart.emails_clients_db.model.dto.AddCategoryDTO;
+import casaart.emails_clients_db.model.dto.AddClientDTO;
 import casaart.emails_clients_db.model.dto.CategoryDTO;
+import casaart.emails_clients_db.model.enums.SourceType;
 import casaart.emails_clients_db.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -37,4 +43,42 @@ public class CategoryController {
 
         return "clients";
     }
+
+    //create new category
+    @GetMapping("/add-category")
+    public String viewAddCategoryForm(Model model) {
+        model.addAttribute("sourceType", SourceType.values());
+
+        if (!model.containsAttribute("isExistCategory")) {
+            model.addAttribute("isExistCategory", false);
+        }
+
+        return "add-category";
+    }
+
+//    @PostMapping("/add-client")
+//    public String addClient(
+//            @Valid AddClientDTO addClientDTO,
+//            BindingResult bindingResult,
+//            RedirectAttributes rAtt, Model model) {
+//
+//        if (bindingResult.hasErrors()) {
+//            rAtt.addFlashAttribute("addClientDTO", addClientDTO);
+//            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.addClientDTO", bindingResult);
+//
+//            return "redirect:/add-client";
+//        }
+//
+//        if (clientService.isExistClientEmail(addClientDTO.getEmail())) {
+//            rAtt.addFlashAttribute("addClientDTO", addClientDTO);
+//            rAtt.addFlashAttribute("isExistEmail", true);
+//            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.addClientDTO", bindingResult);
+//
+//            return "redirect:/add-client";
+//        }
+//
+//        clientService.addClient(addClientDTO);
+//
+//        return "redirect:/clients";
+//    }
 }
