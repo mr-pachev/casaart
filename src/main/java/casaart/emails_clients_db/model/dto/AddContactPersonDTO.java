63338f2d476a963857cf0ee0ddac03f1217e5fc4
mr-pachev <mr.pachev@abv.bg>
2025-01-18 16:20:13 +0200

    Added AddContactPersonDTO.

---
+++ src/main/java/casaart/emails_clients_db/model/dto/AddContactPersonDTO.java
@@ -0,0 +1,79 @@
+package casaart.emails_clients_db.model.dto;
+
+import casaart.emails_clients_db.model.entity.Company;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class AddContactPersonDTO {
+    @NotBlank
+    @Size(min = 2, max = 30)
+    private String firstName;
+
+    @Size(max = 30)
+    private String middleName;
+
+    @NotBlank
+    @Size(min = 2, max = 30)
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @Size(max = 10)
+    private String phoneNumber;
+
+    @NotBlank
+    private String company;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
